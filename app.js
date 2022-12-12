40da26ab3492785b7ba52cad2afb5cbbb66260d2
Ali Ma3ter <AliMa3ter@gmail.com>
2022-12-13 02:09:53 +0330

    HTML & CSS Edited JS Added

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const contextMenu = document.querySelector(".container");
-
+const subMenu = document.querySelector(".share-menu");
 document.addEventListener("contextmenu", (e) => {
   e.preventDefault();
+  let x = e.offsetX;
+  let y = e.offsetY;
+  let winWidth = window.innerWidth;
+  let cmWidth = contextMenu.offsetWidth;
+
+  let winHeight = window.innerHeight;
+  let cmHeight = contextMenu.offsetHeight;
+
+  x = x > winWidth - cmWidth ? winWidth - cmWidth : x;
+  y = y > winHeight - cmHeight ? winHeight - cmHeight : y;
+
+  if (x > winWidth - cmWidth - subMenu.offsetWidth) {
+    subMenu.style.left= '-170px'
+  }else{
+    subMenu.style.left= '260px'
+  }
+
+  contextMenu.style.left = `${x}px`;
+  contextMenu.style.top = `${y}px`;
+  contextMenu.style.visibility = "visible";
+});
+document.addEventListener("click", (e) => {
+  contextMenu.style.visibility = "hidden";
 });
